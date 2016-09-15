b1ec55ffc9b15823b4e072104569262f8f433ecf
berglindoma13 <berglindoma13@ru.is>
2016-09-15 18:38:58 +0000

    collision calc stage1

--- core/src/is/ru/berglindoma13/CannonBall.java
+++ core/src/is/ru/berglindoma13/CannonBall.java
@@ -16,6 +16,8 @@ public class CannonBall {
         position.x = cannon.get_cannonPoint().x;
         position.y = cannon.get_cannonPoint().y;
         position.z = cannon.get_cannonPoint().z;
+        direction.x = 0.0f;
+        direction.y = 1.0f;
     }
 
     public void display(int colorLoc){
@@ -32,10 +34,69 @@ public class CannonBall {
         float deltaTime = Gdx.graphics.getDeltaTime();
         position.x += direction.x * deltaTime * 150.0f;
         position.y += direction.y * deltaTime * 150.0f;
+        System.out.println("dir vector.y " + direction.y);
+        System.out.println("position_y" + position.y);
+
+        collision();
+
     }
 
     public void setDirection(float x){
         direction.x = x;
         direction.y = 1;
+    }
+
+    public void collision(){
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        Point topwall = new Point();
+        topwall.x = 0;
+        topwall.y = 768;
+
+        Vector v = new Vector();
+        v.x = -1024;
+        v.y = 0;
+
+        Vector normal = new Vector();
+        normal.x = -v.y;
+        normal.y = v.x;
+
+        Vector b_a = new Vector();
+        b_a.x = (topwall.x - position.x);
+        b_a.y = (topwall.y - position.y);
+
+        float thit = ((normal.x * b_a.x) + (normal.y * b_a.y)) / ((normal.x * direction.x) + (normal.y * direction.y));
+
+        //putting thit and delta into same units
+        float newthit = thit * direction.x + thit*direction.y;
+        float newdelta = deltaTime * direction.x + deltaTime * direction.y;
+
+        if(newthit < newdelta){
+
+            getReflection();
+
+           // System.out.println(position.y);
+        }
+    }
+
+    public void getReflection(){
+        Vector reflection = new Vector();
+        Vector v = new Vector();
+        v.x = -1024;
+        v.y = 0;
+
+        Vector normal = new Vector();
+        normal.x = -v.y;
+        normal.y = v.x;
+        float calc = (2*((direction.x * normal.x) + (direction.y * normal.y)))/((normal.x*normal.x)+(normal.y*normal.y));
+
+        Vector newn = new Vector();
+        newn.x = normal.x*calc;
+        newn.y = normal.y*calc;
+
+        reflection.x = direction.x - newn.x;
+        reflection.y = direction.y - newn.y;
+
+        direction.x = reflection.x;
+        direction.y = reflection.y;
     }
 }
--- core/src/is/ru/berglindoma13/Vector.java
+++ core/src/is/ru/berglindoma13/Vector.java
@@ -8,9 +8,12 @@ public class Vector {
     public float y;
 
     public void normalize(){
-        float length = (float)Math.sqrt(x*x + y*y);
+        /*float length = (float)Math.sqrt(x*x + y*y);
         x = x / length;
         y = y / length;
+        */
+        x = -y;
+        y = x;
     }
 
     public void normalize(float setLength){
