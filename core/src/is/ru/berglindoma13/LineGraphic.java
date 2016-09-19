fbff2076667a4285ab07f614be442cbb9e7ee889
Hauxii <haukur91@gmail.com>
2016-09-19 11:29:52 +0000

    Lines working

--- core/src/is/ru/berglindoma13/LineGraphic.java
+++ core/src/is/ru/berglindoma13/LineGraphic.java
@@ -2,6 +2,7 @@ package is.ru.berglindoma13;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.BufferUtils;
 
 import javax.sound.sampled.Line;
@@ -11,21 +12,30 @@ import java.nio.FloatBuffer;
  * Created by Berglind on 9/16/2016.
  */
 public class LineGraphic {
-    private static FloatBuffer vertexBuffer;
-    private static int vertexPointerLocal;
+    private  FloatBuffer vertexBuffer;
+    private  int vertexPointerLocal;
 
-    public LineGraphic(){};
-
-    public static void drawline(int vertexPointer, Point a, Point b) {
-        LineGraphic.vertexPointerLocal = vertexPointer;
+    public LineGraphic(int vertexPointer, Point a, Point b){
+        vertexPointerLocal = vertexPointer;
 
         float[] array = {a.x,a.y,b.x,b.y};
 
         vertexBuffer = BufferUtils.newFloatBuffer(4);
         vertexBuffer.put(array);
         vertexBuffer.rewind();
+    }
+
+    public void create() {
+
+    }
+
+    public void drawline() {
+        //ModelMatrix.main.pushMatrix();
+        //ModelMatrix.main.setShaderMatrix();
 
         Gdx.gl.glVertexAttribPointer(vertexPointerLocal, 2, GL20.GL_FLOAT, false, 0, vertexBuffer);
-        Gdx.gl.glDrawArrays(GL20.GL_LINE_STRIP, 0, 2);
+        Gdx.gl.glDrawArrays(GL20.GL_LINES, 0, 2);
+//        ModelMatrix.main.popMatrix();
+
     }
 }
